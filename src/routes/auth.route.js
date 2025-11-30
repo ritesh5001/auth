c7d52718d61195d4f7db07c65c860f936442bfb0
ritesh5001 <rgiri5001@gmail.com>
2025-12-01 01:57:10 +0530

    implemented JWT token

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const userModel = require('../Model/user.model')
+const jwt = require('jsonwebtoken')
 
 router = express.Router()
 
@@ -9,9 +10,13 @@ router.post('/register',async(req,res)=>{
     const user = await userModel.create({
         username,password
     })
+
+const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+
     res.status(201).json({
         message:"User Registered Succesfully",
-        user
+        user,
+        token
     })
 })
 
@@ -39,7 +44,36 @@ router.post('/login',async(req,res)=>{
     res.status(201).json({
         message:"User LoggedIn Successfully"
     })
-    
+
+})
+
+router.get('/user',async(req,res)=>{
+    const{token} = req.body
+
+    if(!token){
+       return res.status(401)({
+        message:"unthorisez access"
+       })
+    }
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        
+        const user = await userModel.findOne({
+            _id:decoded.id
+        })
+
+        res.status(200).json({
+            message:"user data fatched Successfully",
+            user
+        })
+
+    } catch (error) {
+        return res.status(401).json({
+            message:"Unathorized Invalid User"
+        })
+    }
+
 })
 
 module.exports = router
