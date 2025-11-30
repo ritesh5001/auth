d140d527eb0945806c8fab7d45a02a3b49d14f7e
ritesh5001 <rgiri5001@gmail.com>
2025-11-30 22:08:57 +0530

    initislization

--- src/app.js
+++ src/app.js
@@ -1,17 +1,5 @@
-const express = require("express")
-const indexRoutes = require('./routes/index.routes')
-const authRoutes = require('./routes/auth.routes')
+const express = require('express')
 
 const app = express()
 
-app.use(express.json())
-
-app.use((req,res,next)=>{
-    console.log("This is a middleware between App and Route")
-    next()
-})
-
-app.use('/',indexRoutes)
-app.use('/auth',authRoutes)
-
-module.exports = app;
+module.exports = app
